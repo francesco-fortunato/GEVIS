2385cd5191649d0a1bf19b2a55258d3a48e2102e
francesco-fortunato <francesco.fortunato1999@gmail.com>
2024-06-26 03:20:36 +0200

    Implemented Volcano Plot. Update nginx configuration. Implemented backend and frontend processing in R for pval_adj conversion to string (Issue on some p-values recognized as 0).

--- GEVIS/R/hello.R
+++ GEVIS/R/hello.R
@@ -25,6 +25,31 @@ hello <- function(data) {
 }
 
 
+pval <- function(data, N, M) {
+  # Save the column of genes before removing the last column
+  genes <- data[, ncol(data)]
+
+  # Assuming data, dataN, and dataC are your matrices
+  data <- data[, -ncol(data)]
+
+  # x is the first gene of the data matrix
+  pval <- apply(data, 1, function(x) {
+    t.test(x[1:N], x[(N+1):(M+N)], paired=FALSE)$p.value
+  })
+
+  # Adjustment p-value
+  pval_adj <- p.adjust(pval, method="fdr")
+
+  # Convert pval_adj to character (string)
+  pval_adj_str <- as.character(pval_adj)
+
+  # Add the column of genes back to the result
+  result <- data.frame(Gene = genes, pval_adj = pval_adj_str)
+
+  return(result)
+}
+
+
 pca <- function(data, dataC, dataN) {
   library(FactoMineR)
   library(factoextra)
